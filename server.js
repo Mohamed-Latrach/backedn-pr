aac65183fbd581ca8ac4bf200bf37517f337d8a3
Mohamed Latrach <mohamedlatrach267@gmail.com>
2024-02-17 16:14:33 +0100

    connect a server to mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express()
 
 
+mongoose.connect('mongodb+srv://admin:admin@cluster0.cz0fnpj.mongodb.net/project-backend?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('successfully connected to MongoDB');
+        app.listen(3001,()=>{
+            console.log('App serving on port 3001')
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
-
-app.listen(5000,()=>{
-    console.log('App serving on port 500')
-})
